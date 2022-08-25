4f77f64fb87d74250caa7ae33b62b33c87fe062f
amiar <mohamed.amiar@gmail.com>
2022-08-25 20:00:44 +0200

    corrections

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -20,20 +20,21 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
+
+        //admin
         $admin = new User();
         $admin->setUsername($faker->word(1))
             ->setPassword($this->hasher->hashPassword($admin, "password"))
             ->setEmail('admin@admin.com')
             ->setRoles(["ROLE_ADMIN"]);
         $manager->persist($admin);
+
         for ($u = 0; $u < 20; $u++) {
             $user = new User();
             $user->setPassword($this->hasher->hashPassword($user, "password"))
                 ->setUsername($faker->word(1))
                 ->setEmail($faker->email());
-
             $manager->persist($user);
-
             for ($t = 0; $t < 20; $t++) {
                 $task = new Task();
                 $task->setTitle($faker->word(2))
@@ -42,17 +43,17 @@ class AppFixtures extends Fixture
                 $manager->persist($task);
             }
         }
-        $anonymous = new User();
-        $anonymous->setUsername("anonyme")
-            ->setPassword("")
-            ->setEmail('');
-        $task2 = new Task();
         for ($t = 0; $t < 5; $t++) {
-            $task = new Task();
-            $task->setTitle($faker->word(2))
+            $anonymous = new User();
+            $anonymous->setUsername("anonyme")
+                ->setPassword("anonyme")
+                ->setEmail('anonyme@anonyme.com');
+            $manager->persist($anonymous);
+            $task2 = new Task();
+            $task2->setTitle($faker->word(2))
                 ->setContent($faker->paragraph())
                 ->setUser($anonymous);
-            $manager->persist($task);
+            $manager->persist($task2);
         }
         $manager->flush();
     }
